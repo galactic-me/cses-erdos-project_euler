915c95103b5685e20bc76d049b24c88203f84bb6
Aman | अमन <121532863+beingamanforever@users.noreply.github.com>
2024-09-21 02:24:53 +0530

    Add files via upload

---
+++ CSES/Subarray_Sums_II.cpp
@@ -0,0 +1,44 @@
+/**
+ *    author:  compounding
+ *    created: 2024-09-21 02:22:34
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+#define NeedForSpeed                  \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define int long long
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vi> vvi;
+typedef vector<pair<int, int>> vpi;
+#define f first
+#define s second
+#define endl "\n"
+const int mod = 1000000007;
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+signed main()
+{
+    NeedForSpeed;
+    int n, k;
+    cin >> n >> k;
+    vi a(n);
+    map<int, int> mpp;
+    mpp[0] = 1;
+    int cnt = 0;
+    int prefsum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        prefsum += a[i];
+        if (mpp.find(prefsum - k) != mpp.end())
+        {
+            cnt += mpp[prefsum - k];
+        }
+        mpp[prefsum]++;
+    }
+    cout << cnt << endl;
+    return 0;
+}
