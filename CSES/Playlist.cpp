4ab14534b5dd40d88e8052e19731c879fca76bc1
Aman | अमन <121532863+beingamanforever@users.noreply.github.com>
2024-09-24 01:28:27 +0530

    Add files via upload

---
+++ CSES/Playlist.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+#define NeedForSpeed                  \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define int long long
+#define all(x) (x).begin(), (x).end()
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vi> vvi;
+typedef vector<pair<int, int>> vpi;
+#define f first
+#define s second
+#define endl "\n"
+const int mod = 1000000007;
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int l = 0, maxe = 0;
+    map<int, int> mpp;
+    for (int r = 0; r < n; r++)
+    {
+        if (mpp.find(a[r]) != mpp.end())
+        {
+            l = max(l, mpp[a[r]] + 1);
+        }
+        mpp[a[r]] = r;
+        maxe = max(maxe, r - l + 1);
+    }
+
+    cout << maxe << endl;
+}
+
+signed main()
+{
+    NeedForSpeed;
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
