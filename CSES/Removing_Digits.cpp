6de5c6a44acb9d259f12076894b4620736fcb9a6
Aman | अमन <121532863+beingamanforever@users.noreply.github.com>
2024-10-01 23:59:08 +0530

    Add files via upload

--- CSES/Removing_Digits.cpp
+++ CSES/Removing_Digits.cpp
@@ -1,6 +1,6 @@
 /**
  *    author:  compounding
- *    created: 2024-10-01 23:40:58
+ *    created: 2024-10-01 23:55:00
  **/
 #include <bits/stdc++.h>
 using namespace std;
@@ -22,30 +22,22 @@ const int mod = 1000000007;
 int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
 void solve()
 {
-    int n, x;
-    cin >> n >> x;
-    vi c(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> c[i];
-    }
-    // for ordering we consider the first i coins only
-    // pull dp[i][j] = number of ways to make j using the first i coins
-    // dp[i][j] = dp[i-1][j] + dp[i][j-c[i]]
-    vvi dp(n + 1, vi(x + 1, 0));
-    dp[0][0] = 1;
+    int n;
+    cin >> n;
+    // as its asking for minimum initialize the dp array with 1e9
+    vi dp(n + 1, 1e9);
+    dp[0] = 0; // empty set
+    // pull dp -> use the previous values to calculate the current value
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j <= x; j++)
-        {
-            dp[i][j] = dp[i - 1][j];
-            if (j - c[i - 1] >= 0)
-            {
-                dp[i][j] += dp[i][j - c[i - 1]] %= mod;
-            }
+        int x = i;
+        while (x > 0)
+        { // pull dp
+            dp[i] = min(dp[i], dp[i - (x % (10))] + 1);
+            x /= 10;
         }
     }
-    cout << dp[n][x] << endl;
+    cout << dp[n] << endl;
     return;
 }
 
